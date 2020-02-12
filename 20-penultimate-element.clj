dd2c6ff1abb5f15ef41fa9f0fe2dd68ca22b9749
CarnunMP <admin@carnun.com>
2020-02-12 23:42:08 +0000

    21-22, 24-25; modified 19 and 20

--- 20-penultimate-element.clj
+++ 20-penultimate-element.clj
@@ -2,8 +2,8 @@
 ; (= (__ ["a" "b" "c"]) "b")
 ; (= (__ [[1 2] [3 4]]) [1 2])
 
-(= (second-to-last (list 1 2 3 4 5)) 4) ; => true
-(= (second-to-last ["a" "b" "c"]) "b") ; => true
-(= (second-to-last [[1 2] [3 4]]) [1 2]) ; => true
+(= (penultimate-element (list 1 2 3 4 5)) 4) ; => true
+(= (penultimate-element ["a" "b" "c"]) "b") ; => true
+(= (penultimate-element [[1 2] [3 4]]) [1 2]) ; => true
 
-(defn second-to-last [x] (first (rest (reverse x))))
+(defn penultimate-element [x] (first (rest (reverse x))))
