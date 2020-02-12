7b7f2bcb392b148043b85d423139619813bc4aed
CarnunMP <admin@carnun.com>
2020-02-12 23:52:40 +0000

    71

---
+++ 71-rearranging-code.clj
@@ -0,0 +1,7 @@
+; (= (__ (sort (rest (reverse [2 5 4 1 3 6]))))
+;    (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (__))
+;    5)
+
+(= (last (sort (rest (reverse [2 5 4 1 3 6]))))
+   (-> [2 5 4 1 3 6] (reverse) (rest) (sort) (last))
+   5) ; => true
