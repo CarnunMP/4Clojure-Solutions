dbcdc6d46793ad0ca3012a574562c21aab77f56e
CarnunMP <carnun@hotmail.co.uk>
2020-02-12 22:56:41 +0000

    11-15

---
+++ 12-intro-to-sequences.clj
@@ -0,0 +1,7 @@
+; (= __ (first '(3 2 1)))
+; (= __ (second [2 3 4]))
+; (= __ (last (list 1 2 3)))
+
+(= 3 (first '(3 2 1))) ; => true
+(= 3 (second [2 3 4])) ; => true
+(= 3 (last (list 1 2 3))) ; => true
