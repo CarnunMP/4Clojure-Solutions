7772effc5c933fc1ca6dd42ffeacdc396cc8fad5
CarnunMP <admin@carnun.com>
2020-04-21 13:09:07 +0100

    28 done

---
+++ problems/28-flatten-a-sequence.clj
@@ -0,0 +1,16 @@
+; (= (__ '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+; (= (__ ["a" ["b"] "c"]) '("a" "b" "c"))
+; (= (__ '((((:a))))) '(:a))
+
+; from https://stackoverflow.com/questions/37244971/how-can-i-recursively-flatten-arbitrarily-nested-vectors-and-maps-in-clojure:
+
+(defn my-flatten [x]
+  (if (coll? x)
+    (mapcat my-flatten x)
+    [x]))
+
+; Bad, because it consumes stack?
+
+(= (my-flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6)) ; => true
+(= (my-flatten ["a" ["b"] "c"]) '("a" "b" "c")) ; => true
+(= (my-flatten '((((:a))))) '(:a)) ; => true
