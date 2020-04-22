a2708db87ac813c301266d0daa8da734b1eea7ed
CarnunMP <carnun@hotmail.co.uk>
2020-04-22 21:00:07 +0100

    added cool extra solution to 49

--- problems/49-split-a-sequence.clj
+++ problems/49-split-a-sequence.clj
@@ -5,3 +5,5 @@
 (= ((fn [n s] (list (take n s) (drop n s))) 3 [1 2 3 4 5 6]) [[1 2 3] [4 5 6]]) ; => true
 (= ((fn [n s] (list (take n s) (drop n s))) 1 [:a :b :c :d]) [[:a] [:b :c :d]]) ; => true
 (= ((fn [n s] (list (take n s) (drop n s))) 2 [[1 2] [3 4] [5 6]]) [[[1 2] [3 4]] [[5 6]]]) ; => true
+
+; Another cool solution: (juxt take drop)
