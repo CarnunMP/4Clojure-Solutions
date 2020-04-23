2ec4e807b3f4de5f57d1a196f13d946767b05b4b
CarnunMP <admin@carnun.com>
2020-04-23 21:58:08 +0100

    34 done

---
+++ problems/34-implement-range.clj
@@ -0,0 +1,11 @@
+; (= (__ 1 4) '(1 2 3))
+; (= (__ -2 2) '(-2 -1 0 1))
+; (= (__ 5 8) '(5 6 7))
+
+(fn my-range [n1 n2]
+  (when (< n1 n2)
+    (cons n1 (my-range (inc n1) n2))))
+
+(= (my-range 1 4) '(1 2 3)) ; => true
+(= (my-range -2 2) '(-2 -1 0 1)) ; => true
+(= (my-range 5 8) '(5 6 7)) ; => true
