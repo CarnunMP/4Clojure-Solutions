933e8833276863cf3d73dfe5bb7bcbf9988f0151
CarnunMP <carnun@hotmail.co.uk>
2020-06-15 23:33:35 +0100

    Added alt solution to 81.

--- problems/81-set-intersection.clj
+++ problems/81-set-intersection.clj
@@ -10,3 +10,10 @@
 (= ((fn [s1 s2] (set (filter #(contains? s2 %) s1))) #{0 1 2 3} #{2 3 4 5}) #{2 3}) ; => true
 (= ((fn [s1 s2] (set (filter #(contains? s2 %) s1))) #{0 1 2} #{3 4 5}) #{}) ; => true
 (= ((fn [s1 s2] (set (filter #(contains? s2 %) s1))) #{:a :b :c :d} #{:c :e :a :f :d}) #{:a :c :d}) ; => true
+
+; Whoa! You can use sets themselves as filter predicates!
+; So (comp set filter) also works! :D
+
+(= ((comp set filter) #{0 1 2 3} #{2 3 4 5}) #{2 3}) ; => true
+(= ((comp set filter) #{0 1 2} #{3 4 5}) #{}) ; => true
+(= ((comp set filter) #{:a :b :c :d} #{:c :e :a :f :d}) #{:a :c :d}) ; => true
