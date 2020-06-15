4e9bebad8ea7a0755b097d4beb729afea07d8a72
CarnunMP <carnun@hotmail.co.uk>
2020-06-15 23:30:38 +0100

    81 done

---
+++ problems/81-set-intersection.clj
@@ -0,0 +1,12 @@
+;;; Write a function which returns the intersection of two sets. The intersection is the sub-set of items that each set has in common.
+
+;;; (= (__ #{0 1 2 3} #{2 3 4 5}) #{2 3})
+;;; (= (__ #{0 1 2} #{3 4 5}) #{})
+;;; (= (__ #{:a :b :c :d} #{:c :e :a :f :d}) #{:a :c :d})
+
+(fn [s1 s2]
+  (set (filter #(contains? s2 %) s1)))
+
+(= ((fn [s1 s2] (set (filter #(contains? s2 %) s1))) #{0 1 2 3} #{2 3 4 5}) #{2 3}) ; => true
+(= ((fn [s1 s2] (set (filter #(contains? s2 %) s1))) #{0 1 2} #{3 4 5}) #{}) ; => true
+(= ((fn [s1 s2] (set (filter #(contains? s2 %) s1))) #{:a :b :c :d} #{:c :e :a :f :d}) #{:a :c :d}) ; => true
