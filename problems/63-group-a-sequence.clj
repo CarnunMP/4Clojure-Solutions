faf2e783f2ddfb7e7eed700224c5858111e9f5ff
CarnunMP <admin@carnun.com>
2020-07-09 22:05:00 +0100

    problem 63 done

---
+++ problems/63-group-a-sequence.clj
@@ -0,0 +1,30 @@
+;;; Given a function f and a sequence s, write a function which returns a map. The keys should be the values of f applied to each item in s.
+;;; The value at each key should be a vector of corresponding items in the order they appear in s.
+
+;;; Special Restrictions: group-by
+
+;;; (= (__ #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})
+
+;;; (= (__ #(apply / %) [[1 2] [2 4] [4 6] [3 6]])
+;;;    {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})
+
+;;; (= (__ count [[1] [1 2] [3] [1 2 3] [2 3]])
+;;;   {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]})
+
+(defn my-group-by [f coll] ; [note: 4clojure.com doesn't like defn!]
+  (->> (partition-by f (sort coll))
+       (map #(vector (f (first %)) (vec %)))
+       (into {})))
+
+(= (my-group-by #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]}) ; => true
+
+(= (my-group-by #(apply / %) [[1 2] [2 4] [4 6] [3 6]]) ; => true
+   {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})
+
+(= (my-group-by count [[1] [1 2] [3] [1 2 3] [2 3]])
+  {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]}) ; => true
+
+; Note: the solution works, but two things:
+;   1) I can't take full credit: I have https://www.anthony-galea.com/blog/post/solutions-to-4clojure-easy-problems/, to thank.
+;      Seems Anthony had a similar idea!
+;   2) It feels ad-hoc. Would first sorting the coll work in every case? I suspect not.
